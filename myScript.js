96f14669e53d0cf48d843976aefcd9630ebd7e40
FAZEYNA <fazeynafaye@gmail.com>
2020-12-31 10:24:09 +0000

    first commit

---
+++ myScript.js
@@ -0,0 +1,43 @@
+// $("img").hover(function(){
+//     w = $(this).width()+50;
+//     $(this).css("width", w);
+// },function(){
+//     w = $(this).width()-50;
+//     $(this).css("width", w);
+// }); LI YEUP TRANSFORM SCALE DAKEY DEF
+
+// function ajoute() {
+//   var somme = 0;
+//   for (var i = 0, j = arguments.length; i < j; i++) {
+//     somme += arguments[i];
+//   }
+//   return somme;
+// }
+
+// alert(ajoute(2, 3, 4, 5)); // 14
+// $(".activ").hover(function(){
+//     $(this).css("color","black");
+// })
+// let qte = 0;
+// table = document.getElementById("table");
+
+// $(".btn.shadow-none").click(function(){
+//     qte += 1;
+//     this.prix = document.getElementsByClassName('prix').innerHTML;
+//     this.nom = document.getElementsByClassName('nom').innerText;
+//     this.qte = qte;
+//     tr = document.createElement('tr');
+//     td = document.createElement('td');
+//     td.innerText = this.nom;
+//     tr.appendChild(td);
+
+//     td = document.createElement('td');
+//     td.innerText = this.prix;
+//     tr.appendChild(td);
+
+//     td = document.createElement('td');
+//     td.innerText = this.qte;
+//     tr.appendChild(td);
+
+//     table.appendChild(tr);
+// });
